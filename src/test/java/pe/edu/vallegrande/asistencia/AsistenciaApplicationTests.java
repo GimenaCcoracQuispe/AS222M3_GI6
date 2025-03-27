4b5e3c73bd3a8be4045715805dbfac2f12c1a515
Gimena Ccoracc Quispe <111783689+GimenaCcoracQuispe@users.noreply.github.com>
2025-03-26 20:46:33 -0500

    Update AsistenciaApplicationTests.java

--- src/test/java/pe/edu/vallegrande/asistencia/AsistenciaApplicationTests.java
+++ src/test/java/pe/edu/vallegrande/asistencia/AsistenciaApplicationTests.java
@@ -1,1 +1,9 @@
-
+package pe.edu.vallegrande.asistencia;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest
+class AsistenciaApplicationTests {
+ 	@Test
+ 	void contextLoads() {
+ 	}
+}
