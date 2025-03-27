3d43f8d69e039fd89aaba63903d7936bd631674a
Gimena Ccoracc Quispe <111783689+GimenaCcoracQuispe@users.noreply.github.com>
2025-03-26 20:48:33 -0500

    Update IssueRestController.java

--- src/main/java/pe/edu/vallegrande/asistencia/rest/IssueRestController.java
+++ src/main/java/pe/edu/vallegrande/asistencia/rest/IssueRestController.java
@@ -38,12 +38,12 @@ public class IssueRestController {
     }
 
     @GetMapping("/active")
-    public Flux<Issue> GetActiveIssue() {
+    public Flux<Issue> getActiveIssue() {
         return issueService.findStatus("A");
     }
 
     @GetMapping("/inactive")
-    public Flux<Issue> GetInactiveIssue() {
+    public Flux<Issue> getInactiveIssue() {
         return issueService.findStatus("I");
     }
 
